c5ba41a5ff2f85423e87465996c27c2482ec6c2f
mbreisch <64076923+mbreisch@users.noreply.github.com>
2021-06-07 16:34:34 +0200

    Update ACC.cpp

--- DataModel/ACC.cpp
+++ DataModel/ACC.cpp
@@ -1189,22 +1189,22 @@ void ACC::writePsecData(ofstream& d, vector<int> boardsReadyForRead)
 }
 
 /*---------------------------------New commands--------------------------------------*/
-void setSMA_ON()
+void ACC::setSMA_ON()
 {
 	unsigned int command = 0xFF900001;
 	usb->sendData(command); if(usbcheck==false){writeErrorLog("Send Error");}
 	usleep(1000000);
-	unsigned int command = 0xFF910001;
+	command = 0xFF910001;
 	usb->sendData(command); if(usbcheck==false){writeErrorLog("Send Error");}
 	usleep(1000000);
 }
 
-void setSMA_OFF()
+void ACC::setSMA_OFF()
 {
 	unsigned int command = 0xFF900000;
 	usb->sendData(command); if(usbcheck==false){writeErrorLog("Send Error");}
 	usleep(1000000);
-	unsigned int command = 0xFF910000;
+	command = 0xFF910000;
 	usb->sendData(command); if(usbcheck==false){writeErrorLog("Send Error");}
 	usleep(1000000);
 }
