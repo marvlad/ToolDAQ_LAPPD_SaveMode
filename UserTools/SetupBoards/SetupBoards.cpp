d6d6163f90f4134c4522de225f369f8ee2b94257
mbreisch <64076923+mbreisch@users.noreply.github.com>
2023-01-17 20:21:34 +0100

    Update SetupBoards.cpp

--- UserTools/SetupBoards/SetupBoards.cpp
+++ UserTools/SetupBoards/SetupBoards.cpp
@@ -9,15 +9,15 @@ bool SetupBoards::Initialise(std::string configfile, DataModel &data){
 	//m_variables.Print();
 
 	system("mkdir -p Results");
-    system("mkdir -p LocalLogs");
+    	system("mkdir -p LocalLogs");
 
-    if(!m_variables.Get("versionnumber",versionnumber)) versionnumber="Nan";
+    	if(!m_variables.Get("versionnumber",versionnumber)) versionnumber="Nan";
 	
 	std::cout << "----------------------------------------------------------------------" << std::endl;
 	std::cout << "                                 Hello                                " << std::endl;
 	std::cout << "                         This is version ";std::cout<<versionnumber<<"                    " << std::endl;
 	std::cout << " The latest changes were:                                             " << std::endl;
-    std::cout << " - See changelog.txt                                                  " << std::endl;
+    	std::cout << " - See changelog.txt                                                  " << std::endl;
 	std::cout << "----------------------------------------------------------------------" << std::endl;
 
 	m_data= &data;
@@ -28,23 +28,23 @@ bool SetupBoards::Initialise(std::string configfile, DataModel &data){
 	m_data->conf.receiveFlag=1;
 	m_data->acc= new ACC();
 
-    bool ret = LoadSettings();
-    m_variables.Get("Save",m_data->conf.Savemode);
+	bool ret = LoadSettings();
+	m_variables.Get("Save",m_data->conf.Savemode);
 
-    if(m_data->conf.Savemode==2 || m_data->conf.Savemode==4)
-    {
-        // Make the ANNIEEvent Store if it doesn't exist
-        int recoeventexists = m_data->Stores.count("LAPPDStore");
-        if(recoeventexists==0)
-        {
-            m_data->Stores["LAPPDStore"] = new BoostStore(false,2);
-        }
-    }
+	if(m_data->conf.Savemode==2 || m_data->conf.Savemode==4)
+	{
+		// Make the ANNIEEvent Store if it doesn't exist
+		int recoeventexists = m_data->Stores.count("LAPPDStore");
+		if(recoeventexists==0)
+		{
+			m_data->Stores["LAPPDStore"] = new BoostStore(false,2);
+		}
+	}
 
-    if(!m_variables.Get("TimeoutMax",TimeoutMax)) TimeoutMax=300;
-    if(!m_variables.Get("PrintLinesMax",PrintLinesMax)) PrintLinesMax=50000;
+	if(!m_variables.Get("TimeoutMax",TimeoutMax)) TimeoutMax=300;
+	if(!m_variables.Get("PrintLinesMax",PrintLinesMax)) PrintLinesMax=50000;
 
-    Timeoutcounter = 0;
+	Timeoutcounter = 0;
 
 	return true;
 }
@@ -52,25 +52,25 @@ bool SetupBoards::Initialise(std::string configfile, DataModel &data){
 
 bool SetupBoards::Execute(){
 
-    if(Timeoutcounter>=TimeoutMax)
-    {
-        Timeoutcounter = 0; //Reset the timeout counter 
-        
-        //IF THERE ARE PROBLEMS 
-        //COMMENT FROM HERE -----------
-        m_data->conf.receiveFlag = 1; //Re-init the Setup part uf the tool
+	if(Timeoutcounter>=TimeoutMax)
+	{
+		Timeoutcounter = 0; //Reset the timeout counter 
 
-        //Print debug frame as overwrite
-        int numLines = 0;
-        std::string line;
-        std::ifstream file("./LocalLogs/ACCIF.txt");
-        while(getline(file, line)){numLines++;}
-        file.close();
+		//IF THERE ARE PROBLEMS 
+		//COMMENT FROM HERE -----------
+		m_data->conf.receiveFlag = 1; //Re-init the Setup part uf the tool
 
-        if(numLines<PrintLinesMax && m_verbose>1){PrintDebugFrames();}
-        PrintSettings();
-        //TO HERE -------------
-    }
+		//Print debug frame as overwrite
+		int numLines = 0;
+		std::string line;
+		std::ifstream file("./LocalLogs/ACCIF.txt");
+		while(getline(file, line)){numLines++;}
+		file.close();
+
+		if(numLines<PrintLinesMax && m_verbose>1){PrintDebugFrames();}
+		PrintSettings();
+		//TO HERE -------------
+	}
 
 	bool setupret = false;
 	if(m_data->conf.receiveFlag==1)
@@ -83,48 +83,49 @@ bool SetupBoards::Execute(){
 		{
 			m_data->acc->resetACDC();
 		}
-        setupret = Setup();
+        	setupret = Setup();
 	}else if(m_data->conf.receiveFlag==2)
-    {
-        //
-    }else
-    {
-        std::cout << "Setup failed miserably!" << std::endl;
-        return false;
-    }
+	{
+		//
+	}else
+	{
+		std::cout << "Setup failed miserably!" << std::endl;
+		return false;
+	}
 
-    if(m_data->psec.readRetval!=0)
+    	if(m_data->psec.readRetval!=0)
 	{
 		if(m_data->psec.readRetval==404)
 		{
-            Timeoutcounter++;
-            vector<unsigned short> tempV = m_data->acc->getACCInfoFrame();
-            for(int i=0; i<MAX_NUM_BOARDS; i++)
-            {
+            		Timeoutcounter++;
+            		vector<unsigned short> tempV = m_data->acc->getACCInfoFrame();
+			for(int i=0; i<MAX_NUM_BOARDS; i++)
+			{
 				if(tempV.at(16+i)!=PSECFRAME && tempV.at(16+i)!=PPSFRAME)
 				{
-                    if(m_verbose>1){std::cout<<"Data buffers were: "<<i<<" -> "<<tempV.at(16+i)<<std::endl;}
-                    m_data->acc->dumpData(0xFF);
-                    m_data->acc->emptyUsbLine();
-                }
-            }
-            tempV.clear();
-            if(m_verbose>2)
-            {
-                m_data->conf.Print();
-            }
+					if(m_verbose>1){std::cout<<"Data buffers were: "<<i<<" -> "<<tempV.at(16+i)<<std::endl;}
+					m_data->acc->dumpData(0xFF);
+					m_data->acc->emptyUsbLine();
+                		}
+            		}
+            		tempV.clear();
+			if(m_verbose>2)
+			{
+				m_data->conf.Print();
+			}
 		}else
 		{
-            m_data->psec.errorcodes.push_back(0xAA02EE01);
+			m_data->psec.errorcodes.push_back(0xAA02EE01);
+			PrintReturnFrame();
 			m_data->acc->dumpData(0xFF);
-            m_data->acc->emptyUsbLine();
+			m_data->acc->emptyUsbLine();
 		}
 
-        if(m_verbose>1){SaveErrorLog();}
+        	if(m_verbose>1){SaveErrorLog();}
 	}else
-    {
-        Timeoutcounter = 0;
-    }
+	{
+		Timeoutcounter = 0;
+	}
 
 	return true;
 }
@@ -138,108 +139,108 @@ bool SetupBoards::Finalise()
 
 bool SetupBoards::Setup()
 {
-    bool ret=false;
+	bool ret=false;
 
-    int timeout;
-    m_variables.Get("Timeout",timeout);
-    m_data->acc->setTimeoutInMs(timeout);
+	int timeout;
+	m_variables.Get("Timeout",timeout);
+	m_data->acc->setTimeoutInMs(timeout);
 
-    //Map boards to LAPPDs
-    m_data->acc->setLAPPD1(m_data->conf.LAPPDtoBoard1);
-    m_data->psec.LAPPDtoBoard1 = m_data->conf.LAPPDtoBoard1;
-    m_data->acc->setLAPPD2(m_data->conf.LAPPDtoBoard2);
-    m_data->psec.LAPPDtoBoard2 = m_data->conf.LAPPDtoBoard2;
-    m_data->psec.ACC_ID = m_data->conf.ACC_ID;
+	//Map boards to LAPPDs
+	m_data->acc->setLAPPD1(m_data->conf.LAPPDtoBoard1);
+	m_data->psec.LAPPDtoBoard1 = m_data->conf.LAPPDtoBoard1;
+	m_data->acc->setLAPPD2(m_data->conf.LAPPDtoBoard2);
+	m_data->psec.LAPPDtoBoard2 = m_data->conf.LAPPDtoBoard2;
+	m_data->psec.ACC_ID = m_data->conf.ACC_ID;
 
-    //trigger settings
-    ////polarity
-    m_data->acc->setSign(m_data->conf.ACC_Sign, 2);
-    m_data->acc->setSign(m_data->conf.ACDC_Sign, 3);
-    m_data->acc->setSign(m_data->conf.SELF_Sign, 4);
+	//trigger settings
+	////polarity
+	m_data->acc->setSign(m_data->conf.ACC_Sign, 2);
+	m_data->acc->setSign(m_data->conf.ACDC_Sign, 3);
+	m_data->acc->setSign(m_data->conf.SELF_Sign, 4);
 
-    ////self trigger options
-    m_data->acc->setEnableCoin(m_data->conf.SELF_Enable_Coincidence);
+	////self trigger options
+	m_data->acc->setEnableCoin(m_data->conf.SELF_Enable_Coincidence);
 
-    unsigned int coinNum;
-    stringstream ss;
-    ss << std::hex << m_data->conf.SELF_Coincidence_Number;
-    coinNum = std::stoul(ss.str(),nullptr,16);
-    m_data->acc->setNumChCoin(coinNum);
+	unsigned int coinNum;
+	stringstream ss;
+	ss << std::hex << m_data->conf.SELF_Coincidence_Number;
+	coinNum = std::stoul(ss.str(),nullptr,16);
+	m_data->acc->setNumChCoin(coinNum);
 
-    unsigned int threshold;
-    stringstream ss2;
-    ss2 << std::hex << m_data->conf.SELF_threshold;
-    threshold = std::stoul(ss2.str(),nullptr,16);
-    m_data->acc->setThreshold(threshold);
+	unsigned int threshold;
+	stringstream ss2;
+	ss2 << std::hex << m_data->conf.SELF_threshold;
+	threshold = std::stoul(ss2.str(),nullptr,16);
+	m_data->acc->setThreshold(threshold);
 
-    //psec masks combine
-    std::vector<int> PsecChipMask = {m_data->conf.PSEC_Chip_Mask_0,m_data->conf.PSEC_Chip_Mask_1,m_data->conf.PSEC_Chip_Mask_2,m_data->conf.PSEC_Chip_Mask_3,m_data->conf.PSEC_Chip_Mask_4};
-    std::vector<unsigned int> VecPsecChannelMask = {m_data->conf.PSEC_Channel_Mask_0,m_data->conf.PSEC_Channel_Mask_1,m_data->conf.PSEC_Channel_Mask_2,m_data->conf.PSEC_Channel_Mask_3,m_data->conf.PSEC_Channel_Mask_4};
-    m_data->acc->setPsecChipMask(PsecChipMask);
-    m_data->acc->setPsecChannelMask(VecPsecChannelMask);
+	//psec masks combine
+	std::vector<int> PsecChipMask = {m_data->conf.PSEC_Chip_Mask_0,m_data->conf.PSEC_Chip_Mask_1,m_data->conf.PSEC_Chip_Mask_2,m_data->conf.PSEC_Chip_Mask_3,m_data->conf.PSEC_Chip_Mask_4};
+	std::vector<unsigned int> VecPsecChannelMask = {m_data->conf.PSEC_Channel_Mask_0,m_data->conf.PSEC_Channel_Mask_1,m_data->conf.PSEC_Channel_Mask_2,m_data->conf.PSEC_Channel_Mask_3,m_data->conf.PSEC_Channel_Mask_4};
+	m_data->acc->setPsecChipMask(PsecChipMask);
+	m_data->acc->setPsecChannelMask(VecPsecChannelMask);
 
-    //validation window
-    unsigned int validationStart;
-    stringstream ss31;
-    ss31 << std::hex << (int)m_data->conf.Validation_Start/25;
-    validationStart = std::stoul(ss31.str(),nullptr,16);
-    m_data->acc->setValidationStart(validationStart);		
-    
-    unsigned int validationWindow;
-    stringstream ss32;
-    ss32 << std::hex << (int)m_data->conf.Validation_Window/25;
-    validationWindow = std::stoul(ss32.str(),nullptr,16);
-    m_data->acc->setValidationWindow(validationWindow);
+	//validation window
+	unsigned int validationStart;
+	stringstream ss31;
+	ss31 << std::hex << (int)m_data->conf.Validation_Start/25;
+	validationStart = std::stoul(ss31.str(),nullptr,16);
+	m_data->acc->setValidationStart(validationStart);		
 
-    //pedestal set
-    ////set value
-    unsigned int pedestal;
-    stringstream ss4;
-    ss4 << std::hex << m_data->conf.Pedestal_channel;
-    pedestal = std::stoul(ss4.str(),nullptr,16);
-    ////set mask
-    m_data->acc->setPedestals(m_data->conf.ACDC_mask,m_data->conf.Pedestal_channel_mask,pedestal);
+	unsigned int validationWindow;
+	stringstream ss32;
+	ss32 << std::hex << (int)m_data->conf.Validation_Window/25;
+	validationWindow = std::stoul(ss32.str(),nullptr,16);
+	m_data->acc->setValidationWindow(validationWindow);
+
+	//pedestal set
+	////set value
+	unsigned int pedestal;
+	stringstream ss4;
+	ss4 << std::hex << m_data->conf.Pedestal_channel;
+	pedestal = std::stoul(ss4.str(),nullptr,16);
+	////set mask
+	m_data->acc->setPedestals(m_data->conf.ACDC_mask,m_data->conf.Pedestal_channel_mask,pedestal);
 
 
-    //pps settings
-    unsigned int ppsratio;
-    stringstream ss5;
-    ss5 << std::hex << m_data->conf.PPSRatio;
-    ppsratio = std::stoul(ss5.str(),nullptr,16);
-    m_data->acc->setPPSRatio(ppsratio);
-    
-    m_data->acc->setPPSBeamMultiplexer(m_data->conf.PPSBeamMultiplexer);
-    
-    if(m_data->conf.SMA==0)
-    {
-        m_data->acc->setSMA_OFF();
-    }else if(m_data->conf.SMA==1)
-    {
-        m_data->acc->setSMA_ON();
-    }
+	//pps settings
+	unsigned int ppsratio;
+	stringstream ss5;
+	ss5 << std::hex << m_data->conf.PPSRatio;
+	ppsratio = std::stoul(ss5.str(),nullptr,16);
+	m_data->acc->setPPSRatio(ppsratio);
+
+	m_data->acc->setPPSBeamMultiplexer(m_data->conf.PPSBeamMultiplexer);
+
+	if(m_data->conf.SMA==0)
+	{
+		m_data->acc->setSMA_OFF();
+	}else if(m_data->conf.SMA==1)
+	{
+		m_data->acc->setSMA_ON();
+	}
         
-    int retval;
-    retval = m_data->acc->initializeForDataReadout(m_data->conf.triggermode, m_data->conf.ACDC_mask, m_data->conf.Calibration_Mode);
-    if(retval != 0)
-    {
-        std::cout << "Initialization failed!" << std::endl;
-        ret = false;
-    }else
-    {
-        std::cout << "Initialization successfull!" << std::endl;
-        ret = true;
-    }
+	int retval;
+	retval = m_data->acc->initializeForDataReadout(m_data->conf.triggermode, m_data->conf.ACDC_mask, m_data->conf.Calibration_Mode);
+	if(retval != 0)
+	{
+		std::cout << "Initialization failed!" << std::endl;
+		ret = false;
+	}else
+	{
+		std::cout << "Initialization successfull!" << std::endl;
+		ret = true;
+	}
 
-    m_data->conf.receiveFlag = 2;
+	m_data->conf.receiveFlag = 2;
 
-    vector<unsigned int> tmpERR = m_data->acc->returnErrors();
+	vector<unsigned int> tmpERR = m_data->acc->returnErrors();
 	m_data->psec.errorcodes.insert(std::end(m_data->psec.errorcodes), std::begin(tmpERR), std::end(tmpERR));
 	tmpERR.clear();
 
-    m_data->acc->emptyUsbLine();
-    m_data->acc->dumpData(0xFF);
+	m_data->acc->emptyUsbLine();
+	m_data->acc->dumpData(0xFF);
 
-    return ret;
+	return ret;
 }
 
 bool SetupBoards::LoadSettings()
@@ -314,43 +315,44 @@ bool SetupBoards::LoadSettings()
     return true;
 }
 
-void SetupBoards::PrintDebugFrames()
+void SetupBoards::PrintReturnFrame()
 {
-    //Create Debug file
-    std::fstream outfile("./LocalLogs/ACCIF.txt", std::ios_base::out | std::ios_base::app);
+	//Create Debug file
+	std::fstream outfile("./LocalLogs/ReturnFrame.txt", std::ios_base::out | std::ios_base::app);
 
-    //Print a timestamp
-    outfile << "Time: " << m_data->psec.Timestamp << endl;
+	//Print a timestamp
+	outfile << "Time: " << m_data->psec.Timestamp << endl;
+	outfile << "Unexpected return value = " << m_data->psec.readRetval << endl;
 
-    //Grab first ACC info frame and get all buffer sizes that are present
-    vector<unsigned short> PrintFrame = m_data->acc->getACCInfoFrame();
-    for(int j=0; j<8; j++)
-    {
-        if(PrintFrame.at(14) & (1 << j))
-        {
-            outfile << "W" << j << ": " << PrintFrame.at(16+j) << " | ";
-        }
-    }
-    outfile << endl;
+	//Grab first ACC info frame and get all buffer sizes that are present
+	vector<unsigned short> PrintFrame = m_data->acc->getACCInfoFrame();
+	for(int j=0; j<8; j++)
+	{
+		if(PrintFrame.at(14) & (1 << j))
+		{
+			outfile << "W" << j << ": " << PrintFrame.at(16+j) << " | ";
+		}
+	}
+	outfile << endl;
 
-    //Clear temp vector plus sleep a bit
-    PrintFrame.clear();
-    usleep(100000);
+	//Clear temp vector plus sleep a bit
+	PrintFrame.clear();
+	usleep(100000);
 
-    //Grab second ACC info frame and get all buffer sizes that are present
-    PrintFrame = m_data->acc->getACCInfoFrame();
-    for(int j2=0; j2<8; j2++)
-    {
-        if(PrintFrame.at(14) & (1 << j2))
-        {
-            outfile << "W" << j2 << ": " << PrintFrame.at(16+j2) << " | ";
-        }
-    }
-    outfile << endl;
+	//Grab second ACC info frame and get all buffer sizes that are present
+	PrintFrame = m_data->acc->getACCInfoFrame();
+	for(int j2=0; j2<8; j2++)
+	{
+		if(PrintFrame.at(14) & (1 << j2))
+		{
+			outfile << "W" << j2 << ": " << PrintFrame.at(16+j2) << " | ";
+		}
+	}
+	outfile << endl;
 
-    //Close and clear
-    outfile.close();
-    PrintFrame.clear();
+	//Close and clear
+	outfile.close();
+	PrintFrame.clear();
 }
 
 
@@ -393,33 +395,39 @@ void SetupBoards::PrintSettings()
 
 bool SetupBoards::SaveErrorLog()
 {
-    int numLines = 0;
-    std::string line;
-    std::ifstream file("./LocalLogs/Errorlog.txt");    
-    while(getline(file, line)){numLines++;}
-    file.close();
+	int numLines = 0;
+	std::string line;
+	std::ifstream file("./LocalLogs/Errorlog_Setup.txt");    
+	while(getline(file, line)){numLines++;}
+	file.close();
 
-    if(numLines>PrintLinesMax){return false;}
-    if(m_data->psec.errorcodes.size()==1)
-    {
-        if(m_data->psec.errorcodes.at(0)==0x00000000)
-        {
-            return false;
-        }
-    }
+	if(numLines>PrintLinesMax){return false;}
+	if(m_data->psec.errorcodes.size()==0){return false;}
+	if(m_data->psec.errorcodes.size()==1)
+	{
+		if(m_data->psec.errorcodes.at(0)==0x00000000)
+		{
+			m_data->psec.errorcodes.clear();
+		    	return false;
+		}
+	}
 
-    //Create Debug file
-    std::fstream outfile("./LocalLogs/Errorlog.txt", std::ios_base::out | std::ios_base::app);
+	//Create Debug file
+	std::fstream outfile("./LocalLogs/Errorlog_Setup.txt", std::ios_base::out | std::ios_base::app);
 
-    //Print a timestamp
-    outfile << "Time: " << m_data->psec.Timestamp << endl;
-    for(int k1=0; k1<m_data->psec.errorcodes.size(); k1++)
-    {
-        outfile << m_data->psec.errorcodes.at(k1) << " ";
-    }
-    outfile << endl;
+	//Print a timestamp
+	outfile << "Time: " << m_data->psec.Timestamp << endl;
+	for(int k1=0; k1<m_data->psec.errorcodes.size(); k1++)
+	{
+		if(m_data->psec.errorcodes.at(k1)!=0x00000000)
+		{
+			outfile << "0x" << std::hex << m_data->psec.errorcodes.at(k1) << std::dec <<  " ";
+		}
+	}
+	outfile << endl;
+	outfile.close();
 
-    outfile.close();
+	m_data->psec.errorcodes.clear();
 
-    return true;
+	return true;
 }
